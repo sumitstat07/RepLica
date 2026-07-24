9000e18ff803ff4f0788499247cb0bcd0a3678d8
Sumit Sana <sumitsana2002@gmail.com>
2026-07-24 07:54:21 +0530

    Initial commit

---
+++ services/auth/login_wall.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from services.persistence.exercise_repository import get_or_create_user
+
+
+def render_login_wall():
+    if st.session_state.get("user_id") is not None:
+        return True
+    
+    st.title("RepLica : AI Real-time GYM Trainer")
+    st.markdown("### Welcome! Please enter a username to start")
+
+    with st.form("login_form",clear_on_submit=False):
+        username=st.text_input("Name (Unique)",placeholder="e.g : rahulkumar")
+        submit_button=st.form_submit_button("Start Session",width="stretch")
+
+
+    if submit_button:
+        if not username:
+            st.error("Name can't be empty")
+            return False
+        
+        user=get_or_create_user(username)
+
+
+        st.session_state["user_id"]=user["id"]        
+        st.session_state["username"]=user["username"]
+        
+
+        st.rerun()
+    
+    return False
+    
+
